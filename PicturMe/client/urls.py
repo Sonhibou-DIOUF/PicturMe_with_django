8c010e937d26db597957e5e31265f20367075719
Sonhibou DIOUF <hiboudiouf97@gmail.com>
2025-05-26 16:57:14 +0200

    creation de la navbar

--- PicturMe/client/urls.py
+++ PicturMe/client/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('add/', views.add_client, name='add_client'),
-path("<int:id>/" ,views.one_client , name='all_client'),
+path("<int:id>/" ,views.one_client , name='one_client'),
     path('all', views.all_clients, name='all_clients' ),
 ]
--- PicturMe/picture/urls.py
+++ PicturMe/picture/urls.py
@@ -3,6 +3,6 @@ from . import views
 urlpatterns = [
     path('add/', views.add_picture, name='add_picture'),
     path('<int:id>', views.one_picture , name='one_picture' ),
-    path('all/',views.all_pictures , name='all_picture' ),
+    path('all/',views.all_pictures , name='all_pictures' ),
 
 ]
